8e0b7b0ea9c9cb24e07bded5e499730eb5fa8fc6
Billy palacio <bpalacio@assanet.com>
2023-01-19 18:32:06 -0600

    random splash screen

--- src/components/SplashScreen/Index.jsx
+++ src/components/SplashScreen/Index.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export const SplashScreen = () => {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
   return (
-    <div>SplashScreen</div>
-  )
-}
+    <div className="relative min-h-screen flex items-center justify-center">
+      <img
+        src={getRandomInt(10) >= 5 ? "/rainy.svg" : "/sunny.svg"}
+        alt="rainy"
+      />
+    </div>
+  );
+};
